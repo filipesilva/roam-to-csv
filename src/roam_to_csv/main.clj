afd10fa9b8c94419ea75e1bd2abd48c1ffc7e510
Filipe Silva <filipematossilva@gmail.com>
2021-06-28 14:35:05 +0100

    feat: add arg parsing from jayfu

--- src/roam_to_csv/main.clj
+++ src/roam_to_csv/main.clj
@@ -1,5 +1,31 @@
 (ns roam-to-csv.main
+  (:require [clojure.tools.cli :as cli]
+            [datascript.core :as d])
+  ;; Needed for standalone jar to invoke with java
   (:gen-class))
 
-(defn -main [& _args]
-  (println "Hello world!"))
+(def cli-options
+  [["-h" "--help" "Show this message."]])
+
+(defn -main [& args]
+  (let [parsed (cli/parse-opts args cli-options)
+        {:keys [:help]}
+        (:options parsed)]
+    (if help
+      (do
+        (println "Convert a Roam Research EDN export into CSV format.")
+        (println)
+        (println "Usage:")
+        (println "  roam-to-csv backup.edn")
+        (println)
+        (println "Options:")
+        ;; Tools.cli provides a :summary key that can be used for printing:
+        (println (:summary parsed)))
+      (let []
+        (println "Hello world!")
+        #_(prn (func
+              ;; *in* represents a reader from stdin in Clojure
+              ;; json/read can handle readers
+              ;; :key-fn transforms JSON string keys
+              (json/read *in* :key-fn key-fn)))))))
+
