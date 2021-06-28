45277d3e2d7adb80dfc6f2f6577da4f8bf182a3f
Filipe Silva <filipematossilva@gmail.com>
2021-06-28 16:28:11 +0100

    feat: read roam edn and write csv with pages and blocks

--- src/roam_to_csv/main.clj
+++ src/roam_to_csv/main.clj
@@ -1,31 +1,75 @@
 (ns roam-to-csv.main
-  (:require [clojure.tools.cli :as cli]
+  (:require [clojure.string :as str]
+            [clojure.edn :as edn]
+            [clojure.data.csv :as csv]
+            [clojure.tools.cli :as cli]
+            [clojure.java.io :as io]
             [datascript.core :as d])
+  (:import
+   [java.io StringWriter])
   ;; Needed for standalone jar to invoke with java
   (:gen-class))
 
 (def cli-options
   [["-h" "--help" "Show this message."]])
 
-(defn -main [& args]
-  (let [parsed (cli/parse-opts args cli-options)
-        {:keys [:help]}
-        (:options parsed)]
-    (if help
-      (do
-        (println "Convert a Roam Research EDN export into CSV format.")
-        (println)
-        (println "Usage:")
-        (println "  roam-to-csv backup.edn")
-        (println)
-        (println "Options:")
-        ;; Tools.cli provides a :summary key that can be used for printing:
-        (println (:summary parsed)))
-      (let []
-        (println "Hello world!")
-        #_(prn (func
-              ;; *in* represents a reader from stdin in Clojure
-              ;; json/read can handle readers
-              ;; :key-fn transforms JSON string keys
-              (json/read *in* :key-fn key-fn)))))))
+(defn print-help [summary]
+  (println "Convert a Roam Research EDN export into CSV format.")
+  (println "Given ./backup.edn, creates ./backup.csv with pages and blocks.")
+  (println)
+  (println "Usage:")
+  (println "  roam-to-csv ./backup.edn")
+  (println)
+  (println "Options:")
+  ;; Tools.cli provides a :summary key that can be used for printing:
+  (println summary))
 
+(defn read-db [edn-string]
+  (edn/read-string {:readers d/data-readers} edn-string))
+
+(def pages-query
+  '[:find ?page-uid ?title
+    :where
+    [?p :block/uid ?page-uid]
+    [?p :node/title ?title]])
+
+(def blocks-query
+  '[:find  ?block-uid ?parent-uid ?string ?order
+    :where
+    [?b :block/uid ?block-uid]
+    [?b :block/string ?string]
+    [?b :block/order ?order]
+    [?p :block/children ?b]
+    [?p :block/uid ?parent-uid]])
+
+(defn roam-edn->csv-table [edn-string]
+  (let [db       (read-db edn-string)
+        pages    (d/q pages-query db)
+        blocks   (d/q blocks-query db)
+        header   ["uid" "title" "parent" "string" "order"]
+        no-title (fn [[uid & rest]] (apply vector uid nil rest))]
+    (vec (concat [header]
+                 pages
+                 (map no-title blocks)))))
+
+(defn convert [input-filename]
+  (let [output-filename (str/replace input-filename #".edn$" ".csv")
+        string-writer   (StringWriter.)
+        table           (-> input-filename slurp roam-edn->csv-table)]
+    (csv/write-csv string-writer table)
+    (spit output-filename (str string-writer))))
+
+(defn -main [& args]
+  (let [{:keys [options arguments summary]} (cli/parse-opts args cli-options)
+        {:keys [help]}                      options
+        input-filename                      (first arguments)]
+    (cond
+      help                        (print-help summary)
+      (str/blank? input-filename) (println "Invalid filename!")
+      :else                       (convert input-filename))))
+
+(comment
+  (-> "./backup.edn" slurp roam-edn->csv-table)
+  (convert "./backup.edn")
+  ;
+  )
